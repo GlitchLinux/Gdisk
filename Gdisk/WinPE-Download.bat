1daef4e39a5f0c28f9abd09e13848a745ad481aa
GLITCH LINUX <154828412+GlitchLinux@users.noreply.github.com>
2026-07-24 18:46:26 -0800

    Update WinPE-Download.bat

--- Gdisk/WinPE-Download.bat
+++ Gdisk/WinPE-Download.bat
@@ -1,7 +1,7 @@
 @echo off
 title Gdisk WinPE Downloader
 color 0A
-mode con cols=80 lines=8
+mode con cols=80 lines=12
 cd ..
 copy boot\Gdisk-Installer\wget.exe Gdisk\
 cd Gdisk
@@ -11,22 +11,34 @@ del NanoTech-11-x64-v1.8-25-July-2026.wim >NUL
 del NanoTech-11-x64-v1.8.wim >NUL
 cls
 echo.
-echo -----= By : GLITCH LINUX 
-echo -----= Site : https://GITHUB.COM/GLITCHLINUX 
+echo      .aMMMMP   dMMMMb    dMP   .dMMMb    dMP dMP
+echo     dMP'      dMP VMP   amr   dMP' VP   dMP dMP
+echo    dMP MMP'  dMP dMP   dMP    VMMMb    dMMMM'
+echo   dMP.dMP   dMP.aMP   dMP   dP .dMP   dMP'AMF 
+echo   VMMMP'   dMMMMP'   dMP    VMMMP'   dMP dMP 
+echo.
 echo -----= Downloading WinPE Vtoy - EaseUS_Partition_Pro 
 echo.
 wget.exe -q --show-progress "https://glitchlinux.wtf/FILES/Windows-PE/WinPE-Vtoy/EaseUS_Partition_Pro.vtoy" --no-check-certificate
 cls
 echo.
-echo -----= By : GLITCH LINUX 
-echo -----= Site : https://GITHUB.COM/GLITCHLINUX 
+echo      .aMMMMP   dMMMMb    dMP   .dMMMb    dMP dMP
+echo     dMP'      dMP VMP   amr   dMP' VP   dMP dMP
+echo    dMP MMP'  dMP dMP   dMP    VMMMb    dMMMM'
+echo   dMP.dMP   dMP.aMP   dMP   dP .dMP   dMP'AMF 
+echo   VMMMP'   dMMMMP'   dMP    VMMMP'   dMP dMP 
+echo.
 echo -----= Downloading WinPE Vtoy - Diskgenius-Pro-v6
 echo.
 wget.exe -q --show-progress "https://glitchlinux.wtf/FILES/Windows-PE/WinPE-Vtoy/Diskgenius-Pro-v6.vtoy" --no-check-certificate
 cls
 echo.
-echo -----= By : GLITCH LINUX 
-echo -----= Site : https://GITHUB.COM/GLITCHLINUX 
+echo      .aMMMMP   dMMMMb    dMP   .dMMMb    dMP dMP
+echo     dMP'      dMP VMP   amr   dMP' VP   dMP dMP
+echo    dMP MMP'  dMP dMP   dMP    VMMMb    dMMMM'
+echo   dMP.dMP   dMP.aMP   dMP   dP .dMP   dMP'AMF 
+echo   VMMMP'   dMMMMP'   dMP    VMMMP'   dMP dMP 
+echo.
 echo -----= Downloading WinPE Wim - NanoTech-11-x64-v1.8
 echo.
 wget.exe -q --show-progress "https://glitchlinux.wtf/FILES/Windows-PE/NanoTech-11-x64/NanoTech-11-x64-v1.8-25-July-2026.wim" --no-check-certificate
@@ -35,6 +47,12 @@ del NanoTech-11-x64-v1.8-25-July-2026.wim
 del wget.exe
 cls
 echo.
+echo      .aMMMMP   dMMMMb    dMP   .dMMMb    dMP dMP
+echo     dMP'      dMP VMP   amr   dMP' VP   dMP dMP
+echo    dMP MMP'  dMP dMP   dMP    VMMMb    dMMMM'
+echo   dMP.dMP   dMP.aMP   dMP   dP .dMP   dMP'AMF 
+echo   VMMMP'   dMMMMP'   dMP    VMMMP'   dMP dMP 
+echo.
 echo -----= WinPE Downloading Finished 
-echo
+echo.
 pause
