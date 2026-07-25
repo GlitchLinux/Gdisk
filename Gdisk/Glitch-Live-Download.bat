d8eeff1eb54c66bdc161d9ca88cb10230adc9728
GLITCH LINUX <154828412+GlitchLinux@users.noreply.github.com>
2026-07-24 17:28:19 -0800

    Create Glitch-Live-Download.bat

---
+++ Gdisk/Glitch-Live-Download.bat
@@ -0,0 +1,26 @@
+@echo off 
+
+cd ..
+
+mkdir glitch-live
+
+copy boot/Gdisk-Installer/wget.exe glitch-live
+
+cd glitch-live
+
+echo Downloading Glitch-Linux Live v39 - initrd.img
+echo.
+wget.exe -q --show-progress "https://glitchlinux.wtf/ipxe/Glitch-Linux-v39/live/initrd.img" --no-check-certificate
+cls
+echo Downloading Glitch-Linux Live v39 - vmlinuz
+echo.
+wget.exe -q --show-progress "https://glitchlinux.wtf/ipxe/Glitch-Linux-v39/live/vmlinuz" --no-check-certificate
+cls
+echo Downloading Glitch-Linux Live v39 - filesystem.squashfs
+echo.
+wget.exe -q --show-progress "https://glitchlinux.wtf/ipxe/Glitch-Linux-v39/live/filesystem.squashfs" --no-check-certificate
+
+del wget.exe
+
+cls
+echo Glitch Linux Live Sucessfully Downloaded!
