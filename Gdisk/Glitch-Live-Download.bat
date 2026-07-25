fd78847c5cedba22fcda0071e00afd38e4045d3f
GLITCH LINUX <154828412+GlitchLinux@users.noreply.github.com>
2026-07-24 18:30:09 -0800

    Update Glitch-Live-Download.bat

--- Gdisk/Glitch-Live-Download.bat
+++ Gdisk/Glitch-Live-Download.bat
@@ -1,26 +1,45 @@
 @echo off 
+title Glitch-Linux LIVE Download
+color 0A
+mode con cols=80 lines=8
 
 cd ..
 
 mkdir glitch-live
 
-copy boot/Gdisk-Installer/wget.exe glitch-live
+copy boot\Gdisk-Installer\wget.exe glitch-live\
 
 cd glitch-live
 
-echo Downloading Glitch-Linux Live v39 - initrd.img
+del initrd.img >NUL
+del vmlinuz >NUL
+del filesystem.squashfs >NUL
+cls
+echo.
+echo -----= By : GLITCH LINUX 
+echo -----= Site : https://GITHUB.COM/GLITCHLINUX 
+echo -----= Downloading Glitch-Linux Live v39 - initrd.img
 echo.
 wget.exe -q --show-progress "https://glitchlinux.wtf/ipxe/Glitch-Linux-v39/live/initrd.img" --no-check-certificate
 cls
-echo Downloading Glitch-Linux Live v39 - vmlinuz
+echo.
+echo -----= By : GLITCH LINUX 
+echo -----= Site : https://GITHUB.COM/GLITCHLINUX 
+echo -----= Downloading Glitch-Linux Live v39 - vmlinuz
 echo.
 wget.exe -q --show-progress "https://glitchlinux.wtf/ipxe/Glitch-Linux-v39/live/vmlinuz" --no-check-certificate
 cls
-echo Downloading Glitch-Linux Live v39 - filesystem.squashfs
+echo.
+echo -----= By : GLITCH LINUX 
+echo -----= Site : https://GITHUB.COM/GLITCHLINUX 
+echo -----= Downloading Glitch-Linux Live v39 - filesystem.squashfs
 echo.
 wget.exe -q --show-progress "https://glitchlinux.wtf/ipxe/Glitch-Linux-v39/live/filesystem.squashfs" --no-check-certificate
 
 del wget.exe
 
 cls
-echo Glitch Linux Live Sucessfully Downloaded!
+echo.
+echo -----= Glitch Linux Live Sucessfully Downloaded!
+echo.
+pause
