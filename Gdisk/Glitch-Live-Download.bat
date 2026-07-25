4a10ad4bce7eba21983b87d2a7ba846e0c0cefb4
GLITCH LINUX <154828412+GlitchLinux@users.noreply.github.com>
2026-07-24 18:45:12 -0800

    Update Glitch-Live-Download.bat

--- Gdisk/Glitch-Live-Download.bat
+++ Gdisk/Glitch-Live-Download.bat
@@ -1,7 +1,7 @@
 @echo off 
 title Glitch-Linux LIVE Download
 color 0A
-mode con cols=80 lines=8
+mode con cols=80 lines=12
 
 cd ..
 
@@ -16,22 +16,34 @@ del vmlinuz >NUL
 del filesystem.squashfs >NUL
 cls
 echo.
-echo -----= By : GLITCH LINUX 
-echo -----= Site : https://GITHUB.COM/GLITCHLINUX 
-echo -----= Downloading Glitch-Linux Live v39 - initrd.img
+echo      .aMMMMP   dMMMMb    dMP   .dMMMb    dMP dMP
+echo     dMP'      dMP VMP   amr   dMP' VP   dMP dMP
+echo    dMP MMP'  dMP dMP   dMP    VMMMb    dMMMM'
+echo   dMP.dMP   dMP.aMP   dMP   dP .dMP   dMP'AMF 
+echo   VMMMP'   dMMMMP'   dMP    VMMMP'   dMP dMP 
 echo.
+echo -----= Downloading Glitch-Linux Live v39 - initrd.img
+echo. 
 wget.exe -q --show-progress "https://glitchlinux.wtf/ipxe/Glitch-Linux-v39/live/initrd.img" --no-check-certificate
 cls
 echo.
-echo -----= By : GLITCH LINUX 
-echo -----= Site : https://GITHUB.COM/GLITCHLINUX 
+echo      .aMMMMP   dMMMMb    dMP   .dMMMb    dMP dMP
+echo     dMP'      dMP VMP   amr   dMP' VP   dMP dMP
+echo    dMP MMP'  dMP dMP   dMP    VMMMb    dMMMM'
+echo   dMP.dMP   dMP.aMP   dMP   dP .dMP   dMP'AMF 
+echo   VMMMP'   dMMMMP'   dMP    VMMMP'   dMP dMP 
+echo. 
 echo -----= Downloading Glitch-Linux Live v39 - vmlinuz
 echo.
 wget.exe -q --show-progress "https://glitchlinux.wtf/ipxe/Glitch-Linux-v39/live/vmlinuz" --no-check-certificate
 cls
 echo.
-echo -----= By : GLITCH LINUX 
-echo -----= Site : https://GITHUB.COM/GLITCHLINUX 
+echo      .aMMMMP   dMMMMb    dMP   .dMMMb    dMP dMP
+echo     dMP'      dMP VMP   amr   dMP' VP   dMP dMP
+echo    dMP MMP'  dMP dMP   dMP    VMMMb    dMMMM'
+echo   dMP.dMP   dMP.aMP   dMP   dP .dMP   dMP'AMF 
+echo   VMMMP'   dMMMMP'   dMP    VMMMP'   dMP dMP 
+echo.
 echo -----= Downloading Glitch-Linux Live v39 - filesystem.squashfs
 echo.
 wget.exe -q --show-progress "https://glitchlinux.wtf/ipxe/Glitch-Linux-v39/live/filesystem.squashfs" --no-check-certificate
@@ -39,6 +51,12 @@ wget.exe -q --show-progress "https://glitchlinux.wtf/ipxe/Glitch-Linux-v39/live/
 del wget.exe
 
 cls
+echo.
+echo      .aMMMMP   dMMMMb    dMP   .dMMMb    dMP dMP
+echo     dMP'      dMP VMP   amr   dMP' VP   dMP dMP
+echo    dMP MMP'  dMP dMP   dMP    VMMMb    dMMMM'
+echo   dMP.dMP   dMP.aMP   dMP   dP .dMP   dMP'AMF 
+echo   VMMMP'   dMMMMP'   dMP    VMMMP'   dMP dMP 
 echo.
 echo -----= Glitch Linux Live Sucessfully Downloaded!
 echo.
