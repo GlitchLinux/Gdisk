cfdcc1455aaf6c844f42c1a1d4200b9d36741f1c
GLITCH LINUX <154828412+GlitchLinux@users.noreply.github.com>
2026-07-24 18:24:20 -0800

    Update WinPE-Download.bat

--- Gdisk/WinPE-Download.bat
+++ Gdisk/WinPE-Download.bat
@@ -1,23 +1,39 @@
 @echo off
-
+color 0A
+mode con cols=80 lines=8
 cd ..
 copy boot\Gdisk-Installer\wget.exe Gdisk\
 cd Gdisk
+del EaseUS_Partition_Pro.vtoy >NUL
+del Diskgenius-Pro-v6.vtoy >NUL
+del NanoTech-11-x64-v1.8-25-July-2026.wim >NUL 
+del NanoTech-11-x64-v1.8.wim >NUL
 cls
-echo Downloading WinPE Vtoy - EaseUS_Partition_Pro
+echo.
+echo -----= By : GLITCH LINUX 
+echo -----= Site : https://GITHUB.COM/GLITCHLINUX 
+echo -----= Downloading WinPE Vtoy - EaseUS_Partition_Pro 
 echo.
 wget.exe -q --show-progress "https://glitchlinux.wtf/FILES/Windows-PE/WinPE-Vtoy/EaseUS_Partition_Pro.vtoy" --no-check-certificate
 cls
-echo Downloading WinPE Vtoy - Diskgenius-Pro-v6
+echo.
+echo -----= By : GLITCH LINUX 
+echo -----= Site : https://GITHUB.COM/GLITCHLINUX 
+echo -----= Downloading WinPE Vtoy - Diskgenius-Pro-v6
 echo.
 wget.exe -q --show-progress "https://glitchlinux.wtf/FILES/Windows-PE/WinPE-Vtoy/Diskgenius-Pro-v6.vtoy" --no-check-certificate
 cls
-echo Downloading WinPE Wim - NanoTech-11-x64-v1.8
+echo.
+echo -----= By : GLITCH LINUX 
+echo -----= Site : https://GITHUB.COM/GLITCHLINUX 
+echo -----= Downloading WinPE Wim - NanoTech-11-x64-v1.8
 echo.
 wget.exe -q --show-progress "https://glitchlinux.wtf/FILES/Windows-PE/NanoTech-11-x64/NanoTech-11-x64-v1.8-25-July-2026.wim" --no-check-certificate
 copy NanoTech-11-x64-v1.8-25-July-2026.wim NanoTech-11-x64-v1.8.wim
 del NanoTech-11-x64-v1.8-25-July-2026.wim
 del wget.exe
 cls
-echo WinPE Files downloaded sucsessfully!
+echo.
+echo -----= WinPE Downloading Finished 
+echo
 pause
