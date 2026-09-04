0247c539653c4fc540dd7ca88878cc259ba3b5fa
GLITCH LINUX <154828412+GlitchLinux@users.noreply.github.com>
2026-09-04 19:04:15 +0200

    Enhance WinPE-Download.sh with wget options
    Added progress display for wget and improved output clarity.

--- boot/Gdisk-Scripts/WinPE-Download.sh
+++ boot/Gdisk-Scripts/WinPE-Download.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+clear
+cat ../Gdisk-Installer/gdisk.ascii | lolcat
+echo ""
 cd .. && cd ..
 cd Gdisk
 sudo rm -rf MultiTech-11-X64-v2.6.wim
 echo "Downloading MultiTech-11-X64-v2.6.wim"
-sudo wget "https://glitchlinux.wtf/FILES/Windows-PE/MultiTech-11-x64/MultiTech-11-X64-v2.6.wim"
+echo ""
+sudo wget -q --show-progress "https://glitchlinux.wtf/FILES/Windows-PE/MultiTech-11-x64/MultiTech-11-X64-v2.6.wim"
 clear
 echo "MultiTech-11-X64-v2.6.wim Downloaded successfully!" 
 read -p ' '
-
