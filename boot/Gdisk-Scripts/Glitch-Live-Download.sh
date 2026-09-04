e74028d5a85ad9d223427b3ccd11cd16c76ac9a7
GLITCH LINUX <154828412+GlitchLinux@users.noreply.github.com>
2026-09-04 19:02:33 +0200

    Update Glitch-Live-Download.sh

--- boot/Gdisk-Scripts/Glitch-Live-Download.sh
+++ boot/Gdisk-Scripts/Glitch-Live-Download.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-cd ..
-cd ..
+clear
+cat ../Gdisk-Installer/gdisk.ascii | lolcat
+echo ""
+cd .. && cd ..
+cd Gdisk
 sudo mkdir -p glitch-live
 cd glitch-live
 
@@ -9,18 +12,16 @@ sudo rm -rf initrd.img
 sudo rm -rf vmlinuz
 sudo rm -rf filesystem.squashfs
 
-clear
+echo "Downloading Glitch-Linux v42 Live System"
+echo ""
 
-echo "Downloading Glitch-Linux-v42 initrd.img"
 sudo wget -q --show-progress "https://glitchlinux.wtf/ipxe/Glitch-Linux-v42/live/initrd.img"
-clear
-echo "Downloading Glitch-Linux-v42 vmlinuz"
 sudo wget -q --show-progress "https://glitchlinux.wtf/ipxe/Glitch-Linux-v42/live/vmlinuz"
-clear
-echo "Downloading Glitch-Linux-v42 filesystem.squashfs"
 sudo wget -q --show-progress "https://glitchlinux.wtf/ipxe/Glitch-Linux-v42/live/filesystem.squashfs"
 
 clear
+
+cat ../Gdisk-Installer/gdisk.ascii | lolcat
 
 echo "Glitch-Linux-v42 Live-Boot Downloaded!" 
 
